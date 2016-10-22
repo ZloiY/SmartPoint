9e9644428d03100dd77de3d13feb3ab0ea13c560
ZloiY <NoRFolf1000@mail.ru>
2016-10-22 19:58:03 +0300

    greatestLibraryofCentures

---
+++ SmartPoint/Point.cpp
@@ -0,0 +1,83 @@
+#include "Point.h"
+#define BUF_SIZE sizeof(Values)
+using namespace std;
+
+Point::Point()
+{
+	val = new Values;
+	val->x = 0.0;
+	val->y = 0.0;
+	hMapFile = CreateFileMapping(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		BUF_SIZE,
+		szName);
+}
+
+Point::Point(double x, double y)
+{
+	val = new Values;
+	val->x = x;
+	val->y = y;
+	hMapFile = CreateFileMapping(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		BUF_SIZE,
+		szName);
+}
+
+void Point::setX(double x) {
+	val->x = x;
+}
+
+void Point::setY(double y) {
+	val->y = y;
+}
+
+void Point::sendStruct()
+{
+	hMapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS,
+		FALSE,
+		szName);
+	pBuf = (LPTSTR)MapViewOfFile(hMapFile,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		BUF_SIZE);
+	CopyMemory((PVOID)pBuf, val, sizeof(Values));
+	UnmapViewOfFile(pBuf);
+	CloseHandle(hMapFile);
+}
+
+void Point::getStruct()
+{
+	hMapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS,
+		FALSE,
+		szName);
+	pBuf = (LPTSTR)MapViewOfFile(hMapFile,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		BUF_SIZE);
+	CopyMemory(val, (PVOID)pBuf, sizeof(Values));
+	UnmapViewOfFile(pBuf);
+	CloseHandle(hMapFile);
+}
+
+double Point::getX() {
+	return val->x;
+}
+
+double Point::getY() {
+	return val->y;
+}
+
+Point::~Point()
+{
+	CloseHandle(hMapFile);
+	delete val;
+}
---
+++ SmartPoint/Point.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <windows.h> 
+class Point
+{
+public:
+	struct Values
+	{
+		double x, y;
+	};
+	typedef Values* pValues;
+	Point();
+	Point(double x, double y);
+	void setX(double x);
+	void setY(double y);
+	double getX();
+	double getY();
+	void sendStruct();
+	void getStruct();
+	~Point();
+private:
+	HANDLE hMapFile;
+	TCHAR szName[26] = TEXT("Global\MyFileMappingObject");
+	LPCTSTR pBuf;
+	pValues val;
+};
+
